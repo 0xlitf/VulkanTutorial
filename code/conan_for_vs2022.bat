251526af7a28deb59e6804c940e18f29a4b31de6
0xlitf <0xlitf@gmail.com>
2023-09-26 20:25:46 +0800

    mklink /h conan_for_vs2022.bat C:\Users\x\Documents\conan_for_vs2022.bat

--- code/conan_for_vs2022.bat
+++ code/conan_for_vs2022.bat
@@ -1,11 +1,47 @@
-conan profile detect --force
-conan install . --output-folder=./build_for_vs2022 --build=missing -s build_type=Debug
+
+@echo off
+setlocal
+
+set "folderToCheck=./build_for_vs2022"
+
+rmdir /s /q "%folderToCheck%" 2>nul
+
+rem check
+if exist "%folderToCheck%" (
+    echo "%folderToCheck%" still exist, goto :eof
+    
+    pause
+    goto :eof
+) else (
+    echo "%folderToCheck%" not exist
+)
+
+rem conan profile detect --force
+conan install . --output-folder=./build_for_vs2022 --profile=default --build=missing -s build_type=Debug -s compiler.cppstd=23
+conan install . --output-folder=./build_for_vs2022 --profile=default --build=missing -s build_type=Release -s compiler.cppstd=23
 cmake --preset conan-default
 
 cd ./build_for_vs2022
 cmake .. -G "Visual Studio 17 2022" -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake
-cmake --build_for_vs2022 . --config Debug
+cmake --build .
+
+cd ..
+set "scriptPath=%~dp0"
+echo %scriptPath%
+set "scriptPath=%scriptPath:~0,-1%"
+for %%I in ("%scriptPath%") do set "folderName=%%~nI"
+
+set sln="%folderName%.sln"
+echo %sln%
+
+set "linkName=%sln%"
+set "targetFile=build_for_vs2022\%sln%"
+
+echo link: %linkName% to %targetFile%
+mklink %linkName% "%targetFile%"
+
+start "" "%linkName%"
 
 
-
+pause
 
